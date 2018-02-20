ea80134a8297a32ca66398c166465b825b257abb
vermilionsands <sands.vermilion@gmail.com>
2018-02-20 19:51:10 +0100

    Adding initial wrapper over call() from IgniteCompute.

---
+++ src/vermilionsands/ashtree/compute.clj
@@ -0,0 +1,21 @@
+(ns vermilionsands.ashtree.compute
+  (:import [org.apache.ignite.lang IgniteCallable]
+           [org.apache.ignite IgniteCompute]
+           [java.util Collection]))
+
+(deftype IgniteCallableWrapper [f args]
+  IgniteCallable
+  (call [_]
+    (apply f args)))
+
+(defn apply-fn
+  [^IgniteCompute compute f & args]
+  (.call compute ^IgniteCallable (->IgniteCallableWrapper f args)))
+
+(defn apply-fns
+  [^IgniteCompute compute & f-and-args-seq]
+  (.call compute ^Collection (mapv (fn [[f & args]] (->IgniteCallableWrapper f args)) f-and-args-seq)))
+
+(defn apply-seq
+  [^IgniteCompute compute f & args-seqs]
+  (.call compute ^Collection (mapv #(->IgniteCallableWrapper f %) args-seqs)))
