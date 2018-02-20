3ae35a19955e7ee67b4261c829d5d8570115ead1
vermilionsands <sands.vermilion@gmail.com>
2018-02-20 22:41:55 +0100

    Reducing code duplication.

--- src/vermilionsands/ashtree/compute.clj
+++ src/vermilionsands/ashtree/compute.clj
@@ -5,40 +5,33 @@
            [java.util Collection])
   (:gen-class))
 
+;; maybe make this configurable
+(def ^:private caching-eval (memoize/lru eval :lru/threshold 100))
+
 (deftype IgniteCallableWrapper [f args]
   IgniteCallable
   (call [_]
     (apply f args)))
 
-;; maybe make this configurable
-(def ^:private caching-eval (memoize/lru eval :lru/threshold 100))
-
-(deftype EvalIgniteCallableWrapper [f args]
+(deftype EvalIgniteCallableWrapper [fn-form args]
   IgniteCallable
   (call [_]
-    (let [g (caching-eval f)]
-      (apply g args))))
+    (let [f (caching-eval fn-form)]
+      (apply f args))))
+
+(defn ignite-callable [f & args]
+  (cond
+    (list? f) (->EvalIgniteCallableWrapper f args)
+    :else     (->IgniteCallableWrapper f args)))
 
 (defn apply-fn
   [^IgniteCompute compute f & args]
-  (.call compute ^IgniteCallable (->IgniteCallableWrapper f args)))
-
-(defn apply-form
-  [^IgniteCompute compute fn-form & args]
-  (.call compute ^IgniteCallable (->EvalIgniteCallableWrapper fn-form args)))
+  (.call compute ^IgniteCallable (ignite-callable f args)))
 
 (defn apply-fns
   [^IgniteCompute compute & f-and-args-seq]
-  (.call compute ^Collection (mapv (fn [[f & args]] (->IgniteCallableWrapper f args)) f-and-args-seq)))
-
-(defn apply-forms
-  [^IgniteCompute compute & fn-forms-and-args-seq]
- (.call compute ^Collection (mapv (fn [[fn-form & args]] (->EvalIgniteCallableWrapper fn-form args)) fn-forms-and-args-seq)))
+  (.call compute ^Collection (mapv (fn [[f & args]] (ignite-callable f args)) f-and-args-seq)))
 
 (defn apply-seq
   [^IgniteCompute compute f & args-seqs]
-  (.call compute ^Collection (mapv #(->IgniteCallableWrapper f %) args-seqs)))
-
-(defn apply-form-seq
-  [^IgniteCompute compute fn-form & args-seqs]
-  (.call compute ^Collection (mapv #(->EvalIgniteCallableWrapper fn-form %) args-seqs)))
+  (.call compute ^Collection (mapv #(ignite-callable f %) args-seqs)))
