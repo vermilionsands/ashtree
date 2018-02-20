c093d6b6d7e27f00b825ca8a21ec154e88ca374e
vermilionsands <sands.vermilion@gmail.com>
2018-02-20 21:40:29 +0100

    Adding a call variant that takes a function form instead of a function, and evaluates it before calling.
    This is a workaround toenable working with REPL and avoid exeptions when Ignite enounters unknown function (that is - defined in repl).

--- src/vermilionsands/ashtree/compute.clj
+++ src/vermilionsands/ashtree/compute.clj
@@ -9,14 +9,32 @@
   (call [_]
     (apply f args)))
 
+(deftype EvalIgniteCallableWrapper [f args]
+  IgniteCallable
+  (call [_]
+    (let [g (eval f)]
+      (apply g args))))
+
 (defn apply-fn
   [^IgniteCompute compute f & args]
   (.call compute ^IgniteCallable (->IgniteCallableWrapper f args)))
+
+(defn apply-form
+  [^IgniteCompute compute fn-form & args]
+  (.call compute ^IgniteCallable (->EvalIgniteCallableWrapper fn-form args)))
 
 (defn apply-fns
   [^IgniteCompute compute & f-and-args-seq]
   (.call compute ^Collection (mapv (fn [[f & args]] (->IgniteCallableWrapper f args)) f-and-args-seq)))
 
+(defn apply-forms
+  [^IgniteCompute compute & fn-forms-and-args-seq]
+ (.call compute ^Collection (mapv (fn [[fn-form & args]] (->EvalIgniteCallableWrapper fn-form args)) fn-forms-and-args-seq)))
+
 (defn apply-seq
   [^IgniteCompute compute f & args-seqs]
   (.call compute ^Collection (mapv #(->IgniteCallableWrapper f %) args-seqs)))
+
+(defn apply-form-seq
+  [^IgniteCompute compute fn-form & args-seqs]
+  (.call compute ^Collection (mapv #(->EvalIgniteCallableWrapper fn-form %) args-seqs)))
