6c0f95d518bd10e3f1844c2ee3c1a7037953e661
vermilionsands <sands.vermilion@gmail.com>
2018-02-25 09:19:19 +0100

    Add a helper function for compute API instance.

---
+++ src/vermilionsands/ashtree/ignite.clj
@@ -0,0 +1,26 @@
+(ns vermilionsands.ashtree.ignite
+  (:import [org.apache.ignite Ignite]))
+
+(defn compute
+  "Get an instance of compute API.
+
+  Args:
+  instance - Ignite instance
+  opts     - options map
+
+  Options:
+  :async         - if true would enable async mode (deprecated)
+  :cluster-group - ClusterGroup instance group that would be associated with this compute instance
+                   All tasks using this compute instance would be executed on nodes from this cluster group.
+
+  :executor      - name of an executor to be used by this compute instance
+                   All tasks using this compute instance would be processed by this executor. If the executor
+                   does not exist 'public' pool would be used."
+  [^Ignite instance & [opts]]
+  (let [{:keys [async cluster-group executor]} opts
+        compute (if cluster-group
+                  (.compute instance cluster-group)
+                  (.compute instance))]
+    (cond-> compute
+            async         (.withAsync)
+            executor      (.withExecutor executor))))
