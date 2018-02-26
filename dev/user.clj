9ad5ad9df92c7dc875cb48479bbe2a9b73f82d8f
vermilionsands <sands.vermilion@gmail.com>
2018-02-26 22:40:49 +0100

    Move dev helper code to separate ns.

--- dev/user.clj
+++ dev/user.clj
@@ -1,23 +1,5 @@
-(ns user
-  (:require [functions :refer :all]
-            [vermilionsands.ashtree.compute :as c]
-            [vermilionsands.ashtree.data :as d]
-            [vermilionsands.ashtree.function :as f]
-            [vermilionsands.ashtree.ignite :as i])
-  (:import [org.apache.ignite Ignition Ignite IgniteCompute]
-           [org.apache.ignite.cache CacheMode]
-           [org.apache.ignite.configuration IgniteConfiguration AtomicConfiguration]
-           [org.apache.ignite.lang IgniteRunnable IgniteCallable]))
+(ns user)
 
-(defonce ^:dynamic *ignite* nil)
-(defonce ^:dynamic *compute* nil)
-
-(defn ignite! []
-  (let [cfg (doto (IgniteConfiguration.)
-              (.setPeerClassLoadingEnabled true)
-              (.setAtomicConfiguration
-                (doto (AtomicConfiguration.)
-                  (.setCacheMode CacheMode/REPLICATED))))
-        ignite (Ignition/start cfg)]
-    (alter-var-root #'*ignite* (fn [_] ignite))
-    (alter-var-root #'*compute* (constantly (i/compute *ignite*)))))
+(defn dev []
+  (require 'dev)
+  (in-ns 'dev))
