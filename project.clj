5496cb95019945e7d503ce5f7d5a3e621119973a
vermilionsands <sands.vermilion@gmail.com>
2018-02-27 21:54:51 +0100

    Updating description and url in project.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject ashtree "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure + Apache Ignite "
+  :url "https://github.com/vermilionsands/ashtree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.memoize "0.5.9"]
                  [org.apache.ignite/ignite-core "2.3.0"]]
-  :profiles {:dev {:source-paths ["dev"]
-                   :aot [functions vermilionsands.ashtree.compute]}
+  :profiles {:dev  {:source-paths ["dev"]
+                    :aot [functions vermilionsands.ashtree.compute]}
              :test {:aot [vermilionsands.ashtree.test-helpers]}}
   :global-vars {*warn-on-reflection* true})
