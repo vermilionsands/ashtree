be31342d675210af36086ad68e57e77264a1faa8
vermilionsands <sands.vermilion@gmail.com>
2018-03-02 22:22:14 +0100

    Adding helper for executor service.

--- src/vermilionsands/ashtree/ignite.clj
+++ src/vermilionsands/ashtree/ignite.clj
@@ -1,9 +1,11 @@
 (ns vermilionsands.ashtree.ignite
-  (:import [org.apache.ignite Ignite IgniteCompute]))
+  (:import [java.util.concurrent ExecutorService]
+           [org.apache.ignite Ignite IgniteCompute]
+           [org.apache.ignite.cluster ClusterGroup]))
 
 (def ^:dynamic *compute* "Compute API instance to be used with with-compute" nil)
 
-(defn compute
+(defn ^IgniteCompute compute
   "Get an instance of compute API.
 
   Args:
@@ -12,22 +14,33 @@
 
   Options:
   :async         - if true would enable async mode (deprecated)
-  :cluster-group - ClusterGroup instance group that would be associated with this compute instance
+  :cluster       - ClusterGroup instance group that would be associated with this compute instance
                    All tasks using this compute instance would be executed on nodes from this cluster group.
   :executor      - name of an executor to be used by this compute instance
                    All tasks using this compute instance would be processed by this executor. If the executor
                    does not exist 'public' pool would be used."
-  ^IgniteCompute [^Ignite instance & [opts]]
-  (let [{:keys [async cluster-group executor]} opts
-        compute (if cluster-group
-                  (.compute instance cluster-group)
+  [^Ignite instance & [opts]]
+  (let [{:keys [async cluster executor]} opts
+        compute (if cluster
+                  (.compute instance cluster)
                   (.compute instance))]
     (cond-> compute
-            async         (.withAsync)
-            executor      (.withExecutor executor))))
+      async    (.withAsync)
+      executor (.withExecutor executor))))
 
 (defmacro with-compute
   "Evaluates body in a context in which *compute* is bound to a given compute API instance"
   [compute & body]
   `(binding [*compute* ~compute]
      ~@body))
+
+(defn ^ExecutorService executor-service
+  "Get a distributed executor service from Ignite instance
+
+  Args:
+  instance      - Ignite instance
+  cluster-group - optional cluster group that would be used by the executor"
+  [^Ignite instance & [cluster-group]]
+  (if cluster-group
+    (.executorService instance cluster-group)
+    (.executorService instance)))
