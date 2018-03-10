7390e1a53e6f707a3ddabda0635288403c5ba032
vermilionsands <sands.vermilion@gmail.com>
2018-03-10 23:10:09 +0100

    Move clojure to main dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/vermilionsands/ashtree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/core.memoize "0.5.9"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.memoize "0.5.9"]
                  [org.apache.ignite/ignite-core "2.3.0"]]
   :profiles {:dev  {:source-paths ["dev"]
                     :aot [functions]
-                    :dependencies [[org.clojure/clojure "1.9.0"]
-                                   [org.apache.ignite/ignite-log4j "2.3.0"]]
+                    :dependencies [[org.apache.ignite/ignite-log4j "2.3.0"]]
                     :global-vars {*warn-on-reflection* true}}
              :test {:prep-tasks ^replace ["clean" "compile"]
                     :aot [vermilionsands.ashtree.util.functions]}}
